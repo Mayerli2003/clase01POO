b385105c841168ca3f4c2023ab18bad6ee913797
William Diaz <williamefrain1994@gmail.com>
2021-07-19 22:13:35 -0600

    GO

---
+++ firstapppoo/firstapppoo/Negocio/ClsSaludo.cs
@@ -0,0 +1,23 @@
+﻿using firstapppoo.Humano;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace firstapppoo.Negocio
+{
+    class ClsSaludo
+    {
+        //String es una cadena de caracteres  
+
+
+        public String Saludossssss(ClsHumano humano) {
+
+
+            return "Hola mundo" + humano.Nombre;
+        }
+
+
+    }
+}
---
+++ firstapppoo/firstapppoo/Program.cs
@@ -0,0 +1,33 @@
+﻿using firstapppoo.Humano;
+using firstapppoo.Negocio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace firstapppoo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        
+
+
+            ClsSaludo Saludo = new ClsSaludo();
+
+
+            ClsHumano humano = new ClsHumano();
+            humano.Nombre = "William";
+
+          
+
+            Console.WriteLine(Saludo.Saludossssss(humano));
+            Console.ReadLine();
+
+        }
+    }
+}
